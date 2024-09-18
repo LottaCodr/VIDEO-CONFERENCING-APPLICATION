c14e65049e26dbb608636554d98eb3233ba99dae
King-Lota <lottanna47@gmail.com>
2024-09-18 16:24:28 +0100

    Side Bar Implemented

--- constants/index.ts
+++ constants/index.ts
@@ -21,7 +21,7 @@ export const sidebarLinks = [
     },
     {
         label: 'Personal Room',
-        route: '/persona-room',
+        route: '/personal-room',
         imgUrl: '/icons/add-personal.svg'
     },
     
